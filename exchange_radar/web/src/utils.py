9a17a0344220816430b3a91f1b8d5f457c538ccc
Paulo Antunes <pjmlantunes@gmail.com>
2024-01-01 15:16:34 +0100

    Small FE code refactor

--- exchange_radar/web/src/utils.py
+++ exchange_radar/web/src/utils.py
@@ -5,12 +5,12 @@ def get_exchanges(coin: str) -> str:
     txt = []
 
     if coin in settings.BINANCE:
-        txt.append("Binance")
+        txt.append("<span class='binance'>Binance</span>")
     if coin in settings.COINBASE:
-        txt.append("Coinbase")
+        txt.append("<span class='coinbase'>Coinbase</span>")
     if coin in settings.KRAKEN:
-        txt.append("Kraken")
+        txt.append("<span class='kraken'>Kraken</span>")
     if coin in settings.KUCOIN:
-        txt.append("Kucoin")
+        txt.append("<span class='kucoin'>Kucoin</span>")
 
     return ", ".join(txt)
