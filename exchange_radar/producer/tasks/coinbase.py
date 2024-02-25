abafeaaa5b2df90808f60877f3819a5e4be1b4a8
Paulo Antunes <pjmlantunes@gmail.com>
2024-02-25 17:40:22 +0100

    #60 Run a single Coinbase Task for all pairs

--- exchange_radar/producer/tasks/coinbase.py
+++ exchange_radar/producer/tasks/coinbase.py
@@ -14,9 +14,10 @@ ITER_SLEEP = 10.0
 
 
 class CoinbaseTradesTask(Task):
-    async_client = None
+    async def task(self, symbols: tuple[str, ...]):
+        await asyncio.gather(self.process(symbols))
 
-    def process(self, symbol_or_symbols: str | tuple):
+    async def process(self, symbol_or_symbols: str | tuple):
         class CustomClient(Client):
             def on_message(self, message):
                 match message:
@@ -29,24 +30,7 @@ class CoinbaseTradesTask(Task):
                         except Exception as error:
                             logger.error(f"ERROR: {error}")
 
-        while True:
-            try:
-                self.async_client = CustomClient(self.loop, Channel("matches", list(symbol_or_symbols)))
-                break
-            except Exception as error2:
-                logger.error(f"GENERAL ERROR: {error2}")
-                logger.error(f"Trying again in {ITER_SLEEP} seconds...")
-                asyncio.sleep(ITER_SLEEP)
+        CustomClient(self.loop, Channel("matches", list(symbol_or_symbols)))
 
-    def start(self, symbols: tuple[str, ...]):
-        logger.info("Starting Task...")
-        try:
-            self.process(symbol_or_symbols=symbols)
-            self.loop.run_forever()
-        except KeyboardInterrupt:
-            if self.async_client is not None:
-                self.loop.run_until_complete(self.async_client.close())
-            self.loop.close()
-            logger.info("Task was interrupted...")
-        finally:
-            pass
+        while True:
+            await asyncio.sleep(ITER_SLEEP)
